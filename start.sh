a4373ea7519c8f350421225e8c28a16403ce1d03
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2025-06-19 21:38:19 +0100

    feat: Use autossh for tunnel towards OCI

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 autossh -M 0 -N -i "${SSH_TUNNEL_KEY}" \
     -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 \
-    -L 3306:"${SSH_TUNNEL_DESTINATION_HOST_PORT}" "${SSH_TUNNEL_BASTION_USER_HOST}" > /tmp/autossh.log 2>&1 &
+    -L 127.0.0.1:3306:"${SSH_TUNNEL_DESTINATION_HOST_PORT}" "${SSH_TUNNEL_BASTION_USER_HOST}" > /tmp/autossh.log 2>&1 &
 
 python /gwas-indexing/main.py
